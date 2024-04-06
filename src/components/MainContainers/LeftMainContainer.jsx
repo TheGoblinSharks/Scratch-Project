02cb4ffca74e67271131ddb8aae314f3ac8c5c3b
Logan Nelsen <loganjnelsen@gmail.com>
2024-04-06 17:42:31 -0500

    sencondary formatting added

---
+++ src/components/MainContainers/LeftMainContainer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+const LeftMainContainer = () => {
+    return (
+      <div>
+        <div className="left-main-container">
+
+        </div>
+      </div>
+    );
+  };
+  
+  export default LeftMainContainer;
+  
---
+++ src/components/MainContainers/RightMainContainer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+const RightMainContainer = () => {
+    return (
+      <div>
+        <div className="right-main-container">
+
+        </div>
+      </div>
+    );
+  };
+  
+  export default RightMainContainer;
+  
