0be72952b399ea6ea7fbed188c3e33e345609e48
Mike Tagg <mdtagg91@gmail.com>
2024-04-05 20:40:24 -0700

    first steps of styling app level component and container for array display added

---
+++ src/components/ArrayContainer/index.jsx
@@ -0,0 +1,14 @@
+import './index.css'
+
+const ArrayContainer = () => {
+  return (
+    <div className="arrayContainer">
+      <div className="hover"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+      <div className="box"></div>
+    </div>
+  )
+}
+
+export default ArrayContainer
