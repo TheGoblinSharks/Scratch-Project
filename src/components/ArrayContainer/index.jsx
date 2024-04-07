4729281ede3d333134f36cfa2bba0a9b50c149b7
Mike Tagg <mdtagg91@gmail.com>
2024-04-06 19:01:26 -0700

    removed white space

--- src/components/ArrayContainer/index.jsx
+++ src/components/ArrayContainer/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '../../index.css';
 
 const ArrayContainer = ({ arrayData, currentIndex }) => {
+  
   return (
     <div className="arrayContainer">
       <div className="overlay">
