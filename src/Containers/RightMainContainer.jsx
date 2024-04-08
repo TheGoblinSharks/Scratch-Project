e577832bb2e2c9d89fcf42177c50387990108648
TedPham397 <thinhphamart@gmail.com>
2024-04-08 14:03:10 -0700

    added output menu to console log when button clicked

--- src/Containers/RightMainContainer.jsx
+++ src/Containers/RightMainContainer.jsx
@@ -1,44 +1,17 @@
-// RightMainContainer.jsx
-import React, { useState, useEffect } from 'react';
-import ArrayContainer from '../components/ArrayContainer';
+import ArrayContainer from "../components/ArrayContainer";
 
-<<<<<<< HEAD
-const RightMainContainer = () => {
-  const [data, setData] = useState({ array: [], currentIndex: 0 });
-
-  useEffect(() => {
-    // Fetch data from server
-    fetch('/array')
-      .then(response => response.json())
-      .then(data => {
-        setData(data);
-      })
-      .catch(error => {
-        console.error('Error fetching array data:', error);
-      });
-  }, []);
+const RightMainContainer = ({ currentIndex }) => {
+  const arrayData = [0, 1, 2, 3, 4];
 
   return (
-    <div className="right-main-container">
-      <ArrayContainer arrayData={data.array} currentIndex={data.currentIndex} />
-=======
-const RightMainContainer = ({currentIndex}) => {
-  const arrayData = [0, 1, 2, 3, 4]
-  
-  return (
-    <div className="left">
-      <div className="right-main-container">
-        <div className="animation-wrapper">
-          <ArrayContainer  
-            arrayData={arrayData} 
-            currentIndex={currentIndex} 
-          />
+    <div className='left'>
+      <div className='right-main-container'>
+        <div className='animation-wrapper'>
+          <ArrayContainer arrayData={arrayData} currentIndex={currentIndex} />
         </div>
       </div>
->>>>>>> dev
     </div>
-
   );
 };
-  
+
 export default RightMainContainer;
